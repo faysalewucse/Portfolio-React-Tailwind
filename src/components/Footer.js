77162cf9aa80090f4db60b4c4ede21732b1caefb
faysalewucse <faysal.ewucse@gmail.com>
2023-05-25 23:13:32 +0600

    footer design changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,19 @@ import SocialMedias from "../utils/SocialMedias";
 
 export default function Footer() {
   return (
-    <footer id="footer-section">
-      <div className="bg-gray-900 text-white py-12">
-        <div className="row text-center">
-          <div className="col">
-            <h1 className="text-4xl" style={{ fontFamily: "Black Ops One" }}>
-              Faysal Ahmad
-            </h1>
-            <p className="my-5 text-2xl font-serif">
-              Thanks again for watching my website. I’m always active in this
-              social media under included.
-              <br /> So you can connect with me
-            </p>
-            <SocialMedias />
-            <hr className="border border-green-600 w-1/3 mx-auto" />
-            <h6>
-              All Right Reserved - <a href="/">Faysal Ahmad</a>
-            </h6>
-          </div>
-        </div>
+    <footer className="bg-gray-900 text-white py-12">
+      <div className="text-center">
+        <h1 className="text-4xl">Faysal Ahmad</h1>
+        <p className="my-5 text-2xl font-serif">
+          Thanks again for watching my website. I’m always active in this social
+          media under included.
+          <br /> So you can connect with me
+        </p>
+        <SocialMedias extraStyle={"w-fit mx-auto"} />
+        <hr className="border border-green-600 w-1/3 mx-auto my-5" />
+        <h6>
+          All Right Reserved - <a href="/">Faysal Ahmad</a>
+        </h6>
       </div>
     </footer>
   );
--- src/utils/SocialMedias.js
+++ src/utils/SocialMedias.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function SocialMedias() {
+export default function SocialMedias({ extraStyle }) {
   return (
-    <div className=" mt-5 text-white flex gap-2">
+    <div className={`mt-5 text-white flex gap-2 ${extraStyle}`}>
       <a
         href="https://github.com/faysalewucse"
         className="transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
